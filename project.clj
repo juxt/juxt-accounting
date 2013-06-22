e895d8e7f89e0c80445eb832f69db6befd8992f4
Malcolm Sparks <malcolm@juxt.pro>
2013-06-23 00:38:08 +0100

    Incrementing version number ready for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pro.juxt/accounting "0.1.2"
+(defproject pro.juxt/accounting "0.1.3"
   :description "JUXT Accounting"
   :url "https://juxt.pro/accounting"
   :license {:name "Eclipse Public License"
