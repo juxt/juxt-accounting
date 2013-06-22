215e7fb25b17b231a003977d4a2fb7776f8513de
Malcolm Sparks <malcolm@juxt.pro>
2013-06-23 00:23:45 +0100

    Setting Maven group to pro.juxt.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject accounting "0.1.0"
+(defproject pro.juxt/accounting "0.1.1"
   :description "JUXT Accounting"
   :url "https://juxt.pro/accounting"
   :license {:name "Eclipse Public License"
