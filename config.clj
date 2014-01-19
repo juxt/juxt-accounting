23c0ffb0357f5922cb14ce3b6b86c2a81fcfcfcd
Malcolm Sparks <malcolm@juxt.pro>
2014-01-19 23:06:53 +0000

    Moved config file back because it was overriding .jig/config.clj - sanitised for publication

--- config.clj
+++ config.clj
@@ -11,27 +11,16 @@
                     (System/getProperty "user.home")
                     "/src/accounting/project.clj"))
    :db {:uri "datomic:mem://juxt/accounts"}
-   :accounts-file "/home/malcolm/Dropbox.private/JUXT/accounts.edn"
-   #_:db #_{:uri "datomic:mem://congreve/accounts"}
-   #_:accounts-file #_"/home/malcolm/Dropbox.private/Congreve/accounts.edn"
+   ;; Merge in :accounts-file, which needs processing
    }
 
   :accounts/statement-processor
   {:jig/component juxt.accounting.jig/StatementProcessor
    :jig/project #=(eval (str (System/getProperty "user.home") "/src/accounting/project.clj"))
    :jig/dependencies [:accounts/db]
-   :statement-directory "/home/malcolm/Dropbox.private/JUXT/financials/natwest"
    :database :accounts/db
-   :account-mappings {"JOYPLAY LTD" {:credit :joyplay/accounts-receivable}
-                      "GHETTOJEDI" {:debit :ghettojedi/accounts-payable}
-                      "MASTODON C LTD T/A" {:credit :mastodonc/accounts-receivable}
-                      "ANMFS LTD" {:credit :mailonline/accounts-receivable}
-                      "HMRC VAT" {:debit :juxt/vat-liability}
-                      "MOORCROFTC" {:debit :moorcrofts/accounts-payable}
-                      "DRW TRADING GROU" {:credit :drw/accounts-receivable}
-                      "CONGREVE COMPUTING" {:debit :drw/accounts-payable}
-                      }}
-
+   ;; Merge in :statement-directory, where downloaded bank statements can be found (in OFX format)
+   }
 
   :accounts/service
   {:jig/component juxt.accounting.jig/Website
