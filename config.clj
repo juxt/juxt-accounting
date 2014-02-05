bdf55bdd3fe8842c31c207c99b34a3578c653b88
Malcolm Sparks <malcolm@juxt.pro>
2014-02-05 19:57:55 +0000

    Factored out actual details from Jig skeleton config.

--- config.clj
+++ config.clj
@@ -6,55 +6,21 @@
 
   :accounts/db
   {:jig/component juxt.accounting.jig/Database
-   :jig/project #=(eval
-                   (str
-                    (System/getProperty "user.home")
-                    "/src/accounting/project.clj"))
+   :jig/project #=(eval (str (System/getProperty "user.home") "/src/accounting/project.clj"))
    :db {:uri "datomic:mem://juxt/accounts"}
-   }
-
-  :accounts/data-extractor
-  {:jig/component juxt.accounting.jig/DataExtractor
-   :jig/project #=(eval
-                   (str
-                    (System/getProperty "user.home")
-                    "/src/accounting/project.clj"))
-   ;; Merge in :accounts-file, which needs processing
-   }
-
-  :accounts/data-extractor2
-  {:jig/component juxt.accounting.jig/DataExtractor
-   :jig/project #=(eval
-                   (str
-                    (System/getProperty "user.home")
-                    "/src/accounting/project.clj"))
-   ;; Merge in :accounts-file, which needs processing
    }
 
   :accounts/data-loader
   {:jig/component juxt.accounting.jig/DataLoader
-   :jig/project #=(eval
-                   (str
-                    (System/getProperty "user.home")
-                    "/src/accounting/project.clj"))
-   :jig/dependencies [:accounts/db :accounts/data-extractor :accounts/data-extractor2]
-   }
-
-  :accounts/statement-processor
-  {:jig/component juxt.accounting.jig/StatementProcessor
    :jig/project #=(eval (str (System/getProperty "user.home") "/src/accounting/project.clj"))
-   :jig/dependencies [:accounts/data-loader]
-   :database :accounts/db
-   ;; Merge in :statement-directory, where downloaded bank statements can be found (in OFX format)
+   ;; These dependencies can concatenated with DataExtractor components
+   :jig/dependencies [:accounts/db]
    }
 
   :accounts/service
   {:jig/component juxt.accounting.jig/Website
-   :jig/project #=(eval
-                   (str
-                    (System/getProperty "user.home")
-                    "/src/accounting/project.clj"))
-   :jig/dependencies [:accounts/stencil-loader :accounts/statement-processor :accounts/data-loader :accounts/data-extractor]
+   :jig/project #=(eval (str (System/getProperty "user.home") "/src/accounting/project.clj"))
+   :jig/dependencies [:accounts/stencil-loader :accounts/statement-processor :accounts/data-loader]
    :jig.stencil/loader :accounts/stencil-loader
 
    :juxt.accounting/data :accounts/db
@@ -83,8 +49,4 @@
    :jig.web.firefox-reload/host "localhost"
    :jig.web.firefox-reload/port 32000}}
 
- :jig/projects
- [{:jig/project #=(eval
-                   (str
-                    (System/getProperty "user.home")
-                    "/src/accounting/project.clj"))}]}
+ }
